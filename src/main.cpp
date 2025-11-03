d294d652268503cf9250719aa64032adda92b15c
ccn <ccn@cuppajoeman.com>
2025-11-02 19:01:06 -0500

    feat: add in menu

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,3 @@
-#include "utility/resource_path/resource_path.hpp"
 #define STB_IMAGE_IMPLEMENTATION
 #define STB_IMAGE_WRITE_IMPLEMENTATION
 #include <stb_image.h>
@@ -11,6 +10,7 @@
 #include "graphics/texture_packer/texture_packer.hpp"
 
 #include "utility/texture_packer_model_loading/texture_packer_model_loading.hpp"
+#include "utility/resource_path/resource_path.hpp"
 
 #include "system_logic/toolbox_engine/toolbox_engine.hpp"
 
@@ -26,6 +26,8 @@ int main() {
         "mwe_texture_packer",
         {ShaderType::ABSOLUTE_POSITION_WITH_COLORED_VERTEX, ShaderType::TEXTURE_PACKER_CWL_V_TRANSFORMATION_UBOS_1024},
         sound_type_to_file);
+
+    global_logger.remove_all_sinks();
 
     const auto textures_directory = std::filesystem::path("assets");
     std::filesystem::path output_dir = std::filesystem::path("assets") / "packed_textures";
@@ -46,20 +48,14 @@ int main() {
         tbx_engine.batcher.texture_packer_cwl_v_transformation_ubos_1024_shader_batcher.object_id_generator,
         tbx_engine.batcher.texture_packer_cwl_v_transformation_ubos_1024_shader_batcher.ltw_object_id_generator);
 
-    // lightbulb.transform.set_scale(0.01);
-
-    auto [arx, ary] = tbx_engine.window.get_aspect_ratio_in_simplest_terms();
-
-    tbx_engine.shader_cache.set_uniform(ShaderType::ABSOLUTE_POSITION_WITH_COLORED_VERTEX,
-                                        ShaderUniformVariable::ASPECT_RATIO, glm::vec2(arx, ary));
-
     tbx_engine.fps_camera.unfreeze_camera();
     tbx_engine.window.disable_cursor();
 
     auto tick = [&](double dt) {
-        // tbx_engine.process_and_queue_render_input_graphics_sound_menu();
+        auto [arx, ary] = tbx_engine.window.get_aspect_ratio_in_simplest_terms();
 
-        // TODO: draw the lightbulb here
+        tbx_engine.shader_cache.set_uniform(ShaderType::ABSOLUTE_POSITION_WITH_COLORED_VERTEX,
+                                        ShaderUniformVariable::ASPECT_RATIO, glm::vec2(arx, ary));
 
         tbx_engine.shader_cache.set_uniform(ShaderType::TEXTURE_PACKER_CWL_V_TRANSFORMATION_UBOS_1024,
                                             ShaderUniformVariable::CAMERA_TO_CLIP,
@@ -70,6 +66,10 @@ int main() {
                                             tbx_engine.fps_camera.get_view_matrix());
 
         tbx_engine.update_camera_position_with_default_movement(dt);
+
+        tbx_engine.draw_chosen_engine_stats();
+
+        tbx_engine.process_and_queue_render_input_graphics_sound_menu();
 
         tbx_engine.batcher.texture_packer_cwl_v_transformation_ubos_1024_shader_batcher.queue_draw(lightbulb);
 
